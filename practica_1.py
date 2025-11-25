d9faf1fe8ca3532c509cfea54a9e2c3a3b0cbe64
adrian6922 <vargaszambranoj@gmaail.com>
2025-11-25 20:31:51 -0300

    practica de tipos de datos

---
+++ practica_1.py
@@ -0,0 +1,29 @@
+"""
+Crearán varias variables de distintos tipos (entero, flotante, cadena y booleano), las
+modificarán y las utilizarán dentro de un pequeño bloque de código. El objetivo es que
+experimenten con la asignación y manipulación de variables.
+
+Consigna:
+Crear las siguientes variables:
+1- Una variable con el valor de 10.
+2- Una variable con el valor de 7.5.
+3- Una variable con el valor "Matemáticas".
+4- Una variable con el valor True.
+
+Entrada:
+    ingresar un valor tipo entero, flotante, cadena y booleano.
+Proceso:
+    dar los valores a las variables
+Salida:
+    mostrar los valores de las variables
+"""
+entero = 10
+flotante = 7.5
+texto = "Matemáticas"
+estado = True
+print("Tipo de la variable entero:", type(entero))
+print("Tipo de la variable flotante:", type(flotante))
+print("Tipo de la variable texto:", type(texto))
+print("Tipo de la variable estado:", type(estado))
+
+
