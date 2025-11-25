34b40273c90ab5dfe8df54f4cf3f223a156fc4c0
adrian6922 <vargaszambranoj@gmaail.com>
2025-11-25 20:42:49 -0300

    practica 2 operaciones aritmitricas

---
+++ practica_2.py
@@ -0,0 +1,43 @@
+"""
+Contexto: 
+En este ejercicio, practicarán el uso de operadores aritméticos, de comparación y lógicos para
+resolver una serie de problemas simples. El objetivo es que se familiaricen con cómo se
+comportan estos operadores y cómo pueden aplicarse a distintas variables.
+
+Consigna:
+Realiza las siguientes operaciones y guarda el resultado en nuevas variables:
+● Suma a y b, guárdalo en resultado1.
+● Multiplica b por c, guárdalo en resultado2.
+● Divide a por c, guárdalo en resultado3.
+● Calcula el módulo de a dividido entre b, guárdalo en resultado4.
+"""
+#Logica
+"""
+Entrada:
+    crear 3 variables
+Proceso:
+    crear otras variables para guardar los resultados(+, *, /, %)
+Salida:
+    mostrar los resultados
+"""
+
+a = 15
+b = 8
+c = 5
+# suma
+restulado1 = a + b
+
+# multiplicacion
+restulado2 = b * c
+
+# division
+resultado3 = a / c
+
+# modulo
+resultado4 = a % b
+
+# Mostrar resultados(ouput)
+print("El resultado de la suma de a y b es:", restulado1)
+print("El resultado de la multiplicación de b y c es:", restulado2)
+print("El resultado de la división de a entre c es:", resultado3)
+print("El resultado del módulo de a entre b es:", resultado4)
