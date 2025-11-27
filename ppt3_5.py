f3522d2a253046f720ab3b0ba6f71a12dfbb21b4
adrian6922 <vargaszambranoj@gmaail.com>
2025-11-27 19:37:43 -0300

    ejercicio de IMC terminado

--- ppt3_5.py
+++ ppt3_5.py
@@ -19,9 +19,9 @@ Salida:
 
 # condiciones a tener en cuenta
 """
- 1- El peso y la altura no deben ser negativos o cero.
+ 1- El peso y la altura no deben ser negativos o cero. (listo)
  2- El peso y la altura siempre deben ser numeros.
- 3- No se puede ingresar letras o caracteres especiales.
+ 3- No se puede ingresar letras o caracteres especiales. (listo)
  4- Utilizar el operador logico (AND) que valida que ambas condiciones se cumplan al mismo tiempo.
  5- validar que ambos valores son ingresados.(listo)
 """
@@ -42,10 +42,38 @@ while True:
     peso_usuario = input("Ingrese su peso en kilogramos (kg): ")
     altura_usuario = input("Ingrese su altura en metros (m): ")
     if peso_usuario == '' or altura_usuario == '':
-        print(lista_mensajes[4])
-        print(lista_mensajes[6])
+        print(lista_mensajes[4]) # Debe llenar ambos campos
+        print("------------------")
+        print(lista_mensajes[6]) # Intentelo de nuevo
+        print("------------------")
         continue
     # el usuario ingreso los 2 campos con algun dato
+    # validamos que los campos no sean caracteres, valores negativos o cero.
+    elif not peso_usuario.replace('.','',1).isdigit() or not altura_usuario.replace('.','',1).isdigit() or float(peso_usuario) <= 0 or float(altura_usuario) <= 0:
+        print(lista_mensajes[2]) # El peso y la altura deben ser numeros validos
+        print("------------------")
+        print(lista_mensajes[6]) # Intentelo de nuevo
+        print("------------------")
+        continue
+    else:
+        # ambos campos contienen los valores esperados
+        peso_usuario = float(peso_usuario)
+        altura_usuario = float(altura_usuario)
+        break
+
+# Calculo del IMC
+imc = peso_usuario / (altura_usuario ** 2)
+print(f"{lista_mensajes[5]} {imc:.2f}")
+
+# Evaluacion del IMC
+if imc < 18.5:
+    print("Estas por debajo del peso saludable.")
+elif 18.5 <= imc < 24.9:
+    print("Estas en un peso saludable.")
+elif 25 <= imc < 29.9:
+    print("Tienes sobrepeso.")
+else:
+    print("Tienes obesidad.")
     
     
 
